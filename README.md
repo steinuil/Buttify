219f23651b75aa9374122e083d31c8f1e05f3d7c
steenuil <steenuil@olivia.local>
2014-08-09 02:06:58 +0200

    changed name

---
+++ README.md
@@ -0,0 +1,13 @@
+Buttify
+=======
+A Safari extension that replaces all occurrences of "cloud" with "butt".
+
+![If this is what you see you should be fine](http://i.imgur.com/xXwx8Gn.png)
+
+Download
+--------
+Download [here](), or clone/fork this repo and create a package yourself, if you know how to.
+
+Credits
+-------
+Modified by [@steinuil](https://twitter.com/steinuil), inspired by somebody's idea on a certain imageboard, forked from [Leopardize](https://github.com/jokkedk/Leopardize-safari-extension) by Joakim Nygård, [@jokkedk](http://twitter.com/jokkedk).
---
+++ keisterise.safariextension/script.js
@@ -0,0 +1,16 @@
+function recursiveReplace(node) {
+    if (node.nodeType == 3) {
+	node.nodeValue = node.nodeValue.replace(/the cloud/g, "my butt");
+	node.nodeValue = node.nodeValue.replace(/The cloud/g, "My butt");
+	node.nodeValue = node.nodeValue.replace(/the Cloud/g, "my Butt");
+	node.nodeValue = node.nodeValue.replace(/The Cloud/g, "My Butt");
+        node.nodeValue = node.nodeValue.replace(/Cloud/g, "Butt");
+	node.nodeValue = node.nodeValue.replace(/cloud/g, "butt");
+    } else if (node.nodeType == 1) {
+        $(node).contents().each(function () {
+            recursiveReplace(this);
+        });
+    }
+}
+
+recursiveReplace(document.body);
