05820d8cd9d49e084a0f7244b2c75e843b8d79da
steenuil <steenuil.owl@gmail.com>
2017-03-25 16:30:45 +0100

    rewrote function to be less terrible

--- buttify.safariextension/script.js
+++ buttify.safariextension/script.js
@@ -1,16 +1,31 @@
 function replacer(node) {
-	if (node.nodeType == 3) {
-		node.nodeValue = node.nodeValue.replace(/the cloud/g, "my butt");
-		node.nodeValue = node.nodeValue.replace(/The cloud/g, "My butt");
-		node.nodeValue = node.nodeValue.replace(/the Cloud/g, "my Butt");
-		node.nodeValue = node.nodeValue.replace(/The Cloud/g, "My Butt");
-        node.nodeValue = node.nodeValue.replace(/Cloud/g, "Butt");
-		node.nodeValue = node.nodeValue.replace(/cloud/g, "butt");
-	} else if (node.nodeType == 1) {
-		for (var i = 0; i < node.childNodes.length; i++) {
-			replacer(node.childNodes[i]);
-		}
-	}
+  if (node.nodeType == 3) {
+    var matches = node.nodeValue.match(/(the )?cloud/gi);
+    if (!matches) return;
+
+    matches.forEach(function(match) {
+      var len = match.length,
+          str, chars;
+
+      if (len == 9) {
+        str = [ 'm', 'y', ' ', 'b', 'u', 't', 't' ];
+        chars = [ 0, 2, 3, 4, 5, 7, 8 ];
+      } else if (len == 5) {
+        str = [ 'b', 'u', 't', 't' ];
+        chars = [ 0, 1, 3, 4 ];
+      } else return;
+
+      for (var i = 0; i < chars.length; i++) {
+        if (90 > match.charCodeAt(chars[i]) && match.charCodeAt(chars[i]) > 65) {
+          str[i] = str[i].toUpperCase();
+        }
+      }
+
+      node.nodeValue = node.nodeValue.replace(match, str.join(''));
+    });
+  } else if (node.nodeType == 1) {
+    Array.from(node.childNodes).map(function(n) { replacer(n); });
+  }
 }
 
 replacer(document.head);
